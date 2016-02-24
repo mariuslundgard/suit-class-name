05b307c85a43dcb9b5bc8000d0abee1229b6fb15
Marius Lundgård <Marius.Lundgard@nrk.no>
2016-02-24 19:24:34 +0100

    refactor to enable object argument

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,19 +5,31 @@ import {assert} from 'chai'
 
 describe('suitClassName', () => {
   it('should generate a `Block` class name', () => {
-    const cn = suitClassName(null, 'Page')
+    const cn = suitClassName({
+      namespace: null,
+      block: 'Page'
+    })
 
     assert.equal(cn, 'Page')
   })
 
   it('should generate a `Block-element` class name', () => {
-    const cn = suitClassName(null, 'Page', 'title')
+    const cn = suitClassName({
+      namespace: null,
+      block: 'Page',
+      element: 'title'
+    })
 
     assert.equal(cn, 'Page-title')
   })
 
   it('should generate a `Block-element--modifier` class name', () => {
-    const cn = suitClassName(null, 'Page', 'title', 'dark')
+    const cn = suitClassName({
+      namespace: null,
+      block: 'Page',
+      element: 'title',
+      modifier: 'dark'
+    })
 
     assert.equal(cn, 'Page-title--dark')
   })
@@ -29,7 +41,14 @@ describe('suitClassName', () => {
   })
 
   it('should generate a `Block-element--modifier u-util` className', () => {
-    const cn = suitClassName(null, 'Page', 'title', 'dark', null, ['cf'])
+    const cn = suitClassName({
+      namespace: null,
+      block: 'Page',
+      element: 'title',
+      modifier: 'dark',
+      state: null,
+      utils: ['cf']
+    })
 
     assert.equal(cn, 'Page-title--dark u-cf')
   })
